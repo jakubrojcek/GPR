0febad2d73109ac94b16c4ba47b8bf842e1cd8bb
jakubrojcek <jakub.rojcek@gmail.com>
2013-10-03 22:05:52 +0200

    Update LOB_LinkedHashMap.java

--- src/com/jakubrojcek/hftRegulation/LOB_LinkedHashMap.java
+++ src/com/jakubrojcek/hftRegulation/LOB_LinkedHashMap.java
@@ -14,22 +14,22 @@ import java.util.*;
  */
 public class LOB_LinkedHashMap {
     String model;
-    double[] Prices;                           // prices for trading
-    private int[] BookSizes;                   // signed sizes of the book
-    private int[] BookInfo;                    // info used in decision making
-    private int positionShift;                  // ++ if FVup, -- if FVdown
-    double FV;                                 // Fundamental value
-    double tickSize;                           // size of one tick
-    byte nPoints;                              // number of available positions
-    int maxDepth;                              // 0 to 7 which matter
+    double[] Prices; // prices for trading
+    private int[] BookSizes; // signed sizes of the book
+    private int[] BookInfo; // info used in decision making
+    private int positionShift; // ++ if FVup, -- if FVdown
+    double FV; // Fundamental value
+    double tickSize; // size of one tick
+    byte nPoints; // number of available positions
+    int maxDepth; // 0 to 7 which matter
     int maxSumDepth;
-    private int Pt = 0;                        // last transaction position
-    private int b = 0;                         // 1 if last transaction buy, 0 if sell
-    private int OrderID = 0;                   // id stamp for orders used as key in the book LHM
-    ArrayList<Integer> traderIDsHFT     = new ArrayList<Integer>();
-    ArrayList<Integer> traderIDsNonHFT  = new ArrayList<Integer>();
+    private int Pt = 0; // last transaction position
+    private int b = 0; // 1 if last transaction buy, 0 if sell
+    private int OrderID = 0; // id stamp for orders used as key in the book LHM
+    ArrayList<Integer> traderIDsHFT = new ArrayList<Integer>();
+    ArrayList<Integer> traderIDsNonHFT = new ArrayList<Integer>();
     //vectors holding traderIDs, HFT or nonHFT, traderID and position, price and position
-    ArrayList<Order> ActiveOrders       = new ArrayList<Order>();
+    ArrayList<Order> ActiveOrders = new ArrayList<Order>();
     // holds orders which are in the book, remove from book: order.getPosition
     Hashtable<Integer, HashMap<Integer, Integer>> CurrentPosition =
             new Hashtable<Integer, HashMap<Integer, Integer>>();
@@ -118,7 +118,7 @@ public class LOB_LinkedHashMap {
         ArrayList<Integer> tradersExecuted = new ArrayList<Integer>();
         Set keys;
         for (int i = nPoints - 1 - tickChange; i < nPoints - 1; i++){
-            positionShift--;                // TODO: check if shifts the right number of ticks
+            positionShift--; // TODO: check if shifts the right number of ticks
             keys = book[i].keySet();
             if (!keys.isEmpty()){
                 if (book[i].get(keys.iterator().next()).isBuyOrder()){ // executing buyers at (nP - 2)
@@ -135,9 +135,9 @@ public class LOB_LinkedHashMap {
             }
         }
 
-        for (int i = nPoints - 1 - tickChange; i < nPoints; i++){      // TODO: nPoints - tickChange, <=
+        for (int i = nPoints - 1 - tickChange; i < nPoints; i++){ // TODO: nPoints - tickChange, <=
             keys = book[i].keySet();
-            while (!keys.isEmpty()){                        // removing SLOs from the (nP - 1) position
+            while (!keys.isEmpty()){ // removing SLOs from the (nP - 1) position
                 int oID = (Integer) keys.iterator().next();
                 Order o = book[i].remove(oID);
                 int traderID = o.getTraderID();
@@ -155,21 +155,21 @@ public class LOB_LinkedHashMap {
             book[i] = new LinkedHashMap();
             System.arraycopy(Prices, 0, Prices, 1, nPoints - 1);
             Prices[0] = Prices[1] - tickSize;
-            Pt = Math.min(Pt++, nPoints - 1);                        // not to fall of the grid
+            Pt = Math.min(Pt++, nPoints - 1); // not to fall of the grid
         }
         FV = fv;
         BookSizes();
         return tradersExecuted;
     }
-    
+
     public int[] getBookInfo(){
         /* (best Bid, Ask), (depth at B, A), (depth Buy, Sell),
-        (last price, WasBuy) */
+(last price, WasBuy) */
 
-        int Bt;         // best bid position in Prices
-        int At;         // best ask position in Prices
-        int Db = 0;     // depth buys
-        int Ds = 0;     // depth sells
+        int Bt; // best bid position in Prices
+        int At; // best ask position in Prices
+        int Db = 0; // depth buys
+        int Ds = 0; // depth sells
 
 
         int j = 0;
@@ -197,91 +197,95 @@ public class LOB_LinkedHashMap {
         }
         Ds = Math.max(Ds, - maxSumDepth); // limited to half of the LO grid at maxDepth
 
-        BookInfo[0] = Bt;                 // best bid position
-        BookInfo[1] = At;                 // best ask position
-        BookInfo[2] = BookSizes[Bt];      // depth at best bid
-        BookInfo[3] = - BookSizes[At];    // depth at best ask
-        BookInfo[4] = Db;                 // depth buys
-        BookInfo[5] = - Ds;               // depth sells
-        BookInfo[6] = Pt;                 // last transaction position
-        BookInfo[7] = b;                  // 1 if last transaction buy, 0 if sell
+        BookInfo[0] = Bt; // best bid position
+        BookInfo[1] = At; // best ask position
+        BookInfo[2] = BookSizes[Bt]; // depth at best bid
+        BookInfo[3] = - BookSizes[At]; // depth at best ask
+        BookInfo[4] = Db; // depth buys
+        BookInfo[5] = - Ds; // depth sells
+        BookInfo[6] = Pt; // last transaction position
+        BookInfo[7] = b; // 1 if last transaction buy, 0 if sell
 
         return BookInfo;
     }
-       
-     public Integer transactionRule(Integer oID, ArrayList<Order> orders){
-         hist.addOrderData(BookInfo[1] - BookInfo[0]); // quoted spread
-         int pos, size;
-         for (Order o : orders){
-             pos = o.getPosition();
-             size = o.getSize();
-             if (o.isCancelled()){        // TODO: test if this works
-                 int Q = o.getQ();
-                 pos = o.getPosition() - positionShift;
-                 book[pos].remove(o.getOrderID());
-                 ActiveOrders.remove(o);
-                 Collection<Order> collO = book[pos].values();
-                 for (Order order : collO){                             // increase priorities of the remaining orders
-                     Q--;
-                     if (Q < 0){order.increasePriority(size);}
-                 }
-             } else {
-                 if (o.isBuyOrder()){
-                     if (book[pos].size() > 0 && !book[pos].get(book[pos].keySet().iterator().next()).isBuyOrder()){
-                         Order cp = book[pos].remove(book[pos].keySet().iterator().next());
-                         Integer CPid = cp.getTraderID();
-                         traders.get(CPid).execution(FV, o.getTimeStamp());
-                         ActiveOrders.remove(cp);
-                         Pt = pos;                                       // sets last transaction position
-                         b = 1;                                          // sets last transaction direction, buy = 1
-                         hist.addTrade(o, cp, o.getTimeStamp(), Prices[pos], FV);
-                         hist.addOrderData(pos - (double)(BookInfo[1] + BookInfo[0]) / 2); // effective spread
-                         Collection<Order> collO = book[pos].values();
-                         for (Order order : collO){
-                             order.increasePriority(size);
-                         }
-                         oID = CPid;
-                     } else if (pos == nPoints - 1){        // if BMO executed against fringe, just continue
-                     } else{
-                         OrderID++;
-                         o.setOrderID(OrderID);
-                         o.setPosition(pos + positionShift);
-                         o.setQ(book[pos].size());
-                         book[pos].put(OrderID,o);           // put some key number here
-                         ActiveOrders.add(o);
-                         oID = null;
-                     }
 
-                 } else {
-                     if (book[pos].size() > 0 && book[pos].get(book[pos].keySet().iterator().next()).isBuyOrder()){
-                         Order cp = book[pos].remove(book[pos].keySet().iterator().next());
-                         Integer CPid = cp.getTraderID();
-                         traders.get(CPid).execution(FV, o.getTimeStamp());
-                         ActiveOrders.remove(cp);
-                         Pt = pos;           // set last transaction price
-                         b = 0;              // set last transaction direction, 0=sell
-                         hist.addTrade(cp, o, o.getTimeStamp(), Prices[pos], FV);
-                         hist.addOrderData((double) (BookInfo[1] + BookInfo[0]) / 2 - pos);
-                         Collection<Order> collO = book[pos].values();
-                         for (Order order : collO){
-                             order.increasePriority(size);
-                         }
-                         oID = CPid;
-                     } else if (pos == 0){      // if SMO executed against fringe, just continue
-                     } else{
-                         OrderID++;
-                         o.setOrderID(OrderID);
-                         o.setPosition(pos + positionShift);
-                         o.setQ(book[pos].size());
-                         book[pos].put(OrderID, o);           // put some key number here
-                         ActiveOrders.add(o);
-                         oID = null;
-                     }
-                 }
-             }
-         }
-         BookSizes();
-         return oID;
+    public Integer transactionRule(Integer oID, ArrayList<Order> orders){
+        hist.addOrderData(BookInfo[1] - BookInfo[0]); // quoted spread
+        int pos, size;
+        for (Order o : orders){
+            pos = o.getPosition();
+            size = o.getSize();
+            if (o.isCancelled()){ // TODO: test if this works
+                int Q = o.getQ();
+                pos = o.getPosition() - positionShift;
+                Order removed = book[pos].remove(o.getOrderID());
+                if (!ActiveOrders.contains(removed)){
+                    System.out.println("debug");
+                }
+                ActiveOrders.remove(o);
+                Collection<Order> collO = book[pos].values();
+                for (Order order : collO){ // increase priorities of the remaining orders
+                    Q--;
+                    if (Q < 0){order.increasePriority(size);}
+                    // TODO: problem is that the order has no OrderID..
+                }
+            } else {
+                if (o.isBuyOrder()){
+                    if (book[pos].size() > 0 && !book[pos].get(book[pos].keySet().iterator().next()).isBuyOrder()){
+                        Order cp = book[pos].remove(book[pos].keySet().iterator().next());
+                        Integer CPid = cp.getTraderID();
+                        traders.get(CPid).execution(FV, o.getTimeStamp());
+                        ActiveOrders.remove(cp);
+                        Pt = pos; // sets last transaction position
+                        b = 1; // sets last transaction direction, buy = 1
+                        hist.addTrade(o, cp, o.getTimeStamp(), Prices[pos], FV);
+                        hist.addOrderData(pos - (double)(BookInfo[1] + BookInfo[0]) / 2); // effective spread
+                        Collection<Order> collO = book[pos].values();
+                        for (Order order : collO){
+                            order.increasePriority(size);
+                        }
+                        oID = CPid;
+                    } else if (pos == nPoints - 1){ // if BMO executed against fringe, just continue
+                    } else{
+                        OrderID++;
+                        o.setOrderID(OrderID);
+                        o.setPosition(pos + positionShift);
+                        o.setQ(book[pos].size());
+                        book[pos].put(OrderID,o); // put some key number here
+                        ActiveOrders.add(o);
+                        oID = null;
+                    }
+
+                } else {
+                    if (book[pos].size() > 0 && book[pos].get(book[pos].keySet().iterator().next()).isBuyOrder()){
+                        Order cp = book[pos].remove(book[pos].keySet().iterator().next());
+                        Integer CPid = cp.getTraderID();
+                        traders.get(CPid).execution(FV, o.getTimeStamp());
+                        ActiveOrders.remove(cp);
+                        Pt = pos; // set last transaction price
+                        b = 0; // set last transaction direction, 0=sell
+                        hist.addTrade(cp, o, o.getTimeStamp(), Prices[pos], FV);
+                        hist.addOrderData((double) (BookInfo[1] + BookInfo[0]) / 2 - pos);
+                        Collection<Order> collO = book[pos].values();
+                        for (Order order : collO){
+                            order.increasePriority(size);
+                        }
+                        oID = CPid;
+                    } else if (pos == 0){ // if SMO executed against fringe, just continue
+                    } else{
+                        OrderID++;
+                        o.setOrderID(OrderID);
+                        o.setPosition(pos + positionShift);
+                        o.setQ(book[pos].size());
+                        book[pos].put(OrderID, o); // put some key number here
+                        ActiveOrders.add(o);
+                        oID = null;
+                    }
+                }
+            }
+        }
+        BookSizes();
+        return oID;
     }
 
     public void BookSizes(){
@@ -298,11 +302,11 @@ public class LOB_LinkedHashMap {
                 sizeNum += o.getSize();
             }
             /*while (keysO.hasNext()){
-                sizeNum += book[i].get(keysO.next()).getSize();
-            }*/
+sizeNum += book[i].get(keysO.next()).getSize();
+}*/
             if (sizeNum != 0){
-                buy = book[i].get(book[i].keySet().iterator().next()).isBuyOrder();  // buy orders at book[i]?
-                BookSizes[i] = buy ? Math.min(sizeNum, maxDepth) : - Math.min(sizeNum, maxDepth);   // max size at each tick is maxDepth- 7 or 15
+                buy = book[i].get(book[i].keySet().iterator().next()).isBuyOrder(); // buy orders at book[i]?
+                BookSizes[i] = buy ? Math.min(sizeNum, maxDepth) : - Math.min(sizeNum, maxDepth); // max size at each tick is maxDepth- 7 or 15
             }
             orderNum += sizeNum;
             sizeNum = 0;
@@ -311,12 +315,12 @@ public class LOB_LinkedHashMap {
             System.out.println("error");
         }
         /*Iterator trs = traders.keySet().iterator();
-        while (trs.hasNext()){
-            CurrentPosition.get(trs.next()).
-        }*/
+while (trs.hasNext()){
+CurrentPosition.get(trs.next()).
+}*/
     }
 
-    public Order tryCancel(Order o){   // if order is null after returning, see if there's sth to cancel
+    public Order tryCancel(Order o){ // if order is null after returning, see if there's sth to cancel
         int id = o.getTraderID();
         int tempSize = CurrentPosition.get(id).get(o.getPosition());
         boolean buy = o.isBuyOrder();
@@ -359,15 +363,15 @@ public class LOB_LinkedHashMap {
             System.out.println(Prices[i] + ": " + book[i].keySet());
         }
         /*System.out.println("non HFTs " + traderIDsNonHFT);
-        System.out.println("HFTs " + traderIDsHFT);
-        System.out.println(BookInfo[0] + " best bid @ position = " + BookInfo[0] + " shift " + positionShift);
-        System.out.println(BookInfo[1] + " best ask @ position = " + BookInfo[1] + " shift " + positionShift);
-        System.out.println(BookInfo[2] + " depth at best bid");
-        System.out.println(BookInfo[3] + " depth at best ask");
-        System.out.println(BookInfo[4] + " depth buys");
-        System.out.println(BookInfo[5] + " depth sells");
-        System.out.println(BookInfo[6] + " last transaction price");
-        System.out.println(BookInfo[7] + " 1 if last transaction buy, 0 if sell");*/
+System.out.println("HFTs " + traderIDsHFT);
+System.out.println(BookInfo[0] + " best bid @ position = " + BookInfo[0] + " shift " + positionShift);
+System.out.println(BookInfo[1] + " best ask @ position = " + BookInfo[1] + " shift " + positionShift);
+System.out.println(BookInfo[2] + " depth at best bid");
+System.out.println(BookInfo[3] + " depth at best ask");
+System.out.println(BookInfo[4] + " depth buys");
+System.out.println(BookInfo[5] + " depth sells");
+System.out.println(BookInfo[6] + " last transaction price");
+System.out.println(BookInfo[7] + " 1 if last transaction buy, 0 if sell");*/
     }
 
 
